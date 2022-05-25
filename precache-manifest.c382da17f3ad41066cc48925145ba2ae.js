277b0eec86d077785d8914b75e2bdee1851868f7
Daniil <danila.stasiuk@gmail.com>
2022-05-25 11:44:59 +0200

    Updates

---
+++ precache-manifest.c382da17f3ad41066cc48925145ba2ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650480ceebd4e6dd8584bb270ba8d0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db080985e922aacb9123",
+    "url": "/static/css/main.a0ab3751.chunk.css"
+  },
+  {
+    "revision": "a630968cba511403d833",
+    "url": "/static/js/2.8e683c52.chunk.js"
+  },
+  {
+    "revision": "db080985e922aacb9123",
+    "url": "/static/js/main.78c587be.chunk.js"
+  },
+  {
+    "revision": "512a4f888a379d4d7645",
+    "url": "/static/js/runtime-main.1638fc71.js"
+  }
+]);
